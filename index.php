63acd75c85220766a28893b58f3da2d58c161c88
Kevin Johnson <kevin@amplitudedesign.com>
2016-11-02 16:12:41 -0500

    Move intents

--- index.php
+++ index.php
@@ -45,50 +45,10 @@ $response = new \Alexa\Response\Response;
 
 // Handle the intent for an IntentRequest
 if ($alexaRequest instanceof IntentRequest) {
-
     $intent = $rawRequest['request']['intent']['name'];
+
     if ($intent == 'AnswerIntent') {
-        $stopNumber = $rawRequest['request']['intent']['slots']['stopNumber']['value'];
-        $stopNumber = str_replace(array(",", "."), array("",""), $stopNumber);
-        $stopNumber = (int)$stopNumber;
-
-        $responseText = 'You are looking for stop '.$stopNumber;
-
-        if (isset($rawRequest['request']['intent']['slots']['routeNumber']['value'])) {
-            $routeNumber = $rawRequest['request']['intent']['slots']['routeNumber']['value'];
-            $routeNumber = str_replace(array(",", "."), array("",""), $routeNumber);
-            $routeNumber = (int)$routeNumber;
-
-            $responseText .= " and route number ".$routeNumber;
-        }
-        $queryArr['key'] = 'amplitude';
-        $queryArr['stopID'] = $stopNumber;
-        $queryArr['routeID'] = $routeNumber;
-
-        $url = "http://api.smsmybus.com/v1/getarrivals?".http_build_query($queryArr);
-        $reply = file_get_contents($url);
-        $replyArr = json_decode($reply, true);
-        if (APP_DEBUG === true) {
-            error_log($reply);
-        }
-        $responseText = 'Here are the upcoming arrival times for stop '.$stopNumber.'. ';
-        foreach ($replyArr['stop']['route'] as $key => $data) {
-            $data['destination'] = str_replace("CAP SQR", "Capital Square", $data['destination']);
-            $data['destination'] = str_replace("DUNNS MSH", "Dunna Marsh", $data['destination']);
-            $data['destination'] = str_replace("CAP SQR", "Capital Square", $data['destination']);
-            $data['destination'] = str_replace("NORTH TP", "North Transfer Point", $data['destination']);
-            $data['destination'] = str_replace("EAST TP", "East Transfer Point", $data['destination']);
-            $data['destination'] = str_replace("WEST TP", "West Transfer Point", $data['destination']);
-            $data['destination'] = str_replace("SOUTH TP", "South Transfer Point", $data['destination']);
-            
-            $data['destination'] = str_replace("E TOWNE", "East Town", $data['destination']);
-            $data['destination'] = str_replace("WEXFD RG", "Wexford Ridge", $data['destination']);
-            $data['destination'] = str_replace("WESTFLD", "Westfield", $data['destination']);
-            
-            $responseText .= "Route ".$routeNumber." toward ".$data['destination'];
-            $responseText .= " arrives at ".$data['arrivalTime'].". ";
-        }
-        $response->respond($responseText);
+        $responseIntent = new SmsMyBus\Intents\AnswerIntent($response, $rawRequest);
     }
 
     if ($intent == "ThankYouIntent") {
---
+++ src/Intents/AnswerIntent.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace SmsMyBus\Intents;
+
+class AnswerIntent
+{
+
+    private $response;
+
+    private $rawRequest = array();
+
+    public $responseText = '';
+
+    public function __construct(\Alexa\Response\Response &$response, $rawRequest)
+    {
+        $this -> response = $response;
+        $this -> rawRequest = $rawRequest;
+        $this -> setResponse();
+    }
+    public function setResponse()
+    {
+        $stopNumber = $this -> rawRequest['request']['intent']['slots']['stopNumber']['value'];
+        $stopNumber = str_replace(array(",", "."), array("",""), $stopNumber);
+        $stopNumber = (int)$stopNumber;
+
+        $responseText = 'You are looking for stop '.$stopNumber;
+
+        if (isset($this -> rawRequest['request']['intent']['slots']['routeNumber']['value'])) {
+            $routeNumber = $this -> rawRequest['request']['intent']['slots']['routeNumber']['value'];
+            $routeNumber = str_replace(array(",", "."), array("",""), $routeNumber);
+            $routeNumber = (int)$routeNumber;
+            $responseText .= " and route number ".$routeNumber;
+        }
+
+        $queryArr['key'] = 'amplitude';
+        $queryArr['stopID'] = $stopNumber;
+        $queryArr['routeID'] = $routeNumber;
+
+        $url = "http://api.smsmybus.com/v1/getarrivals?".http_build_query($queryArr);
+        $reply = file_get_contents($url);
+        $replyArr = json_decode($reply, true);
+        if (APP_DEBUG === true) {
+            error_log($reply);
+        }
+
+        if (is_array($replyArr['stop']['route'])) {
+            $responseText = $this -> stopHasRoutes($replyArr);
+        }
+
+        $this -> response->respond($responseText);
+    }
+
+    private function stopHasRoutes($replyArr)
+    {
+        $responseText = 'Here are the upcoming arrival times for stop '.$replyArr['stop']['stopID'].'. ';
+        foreach ($replyArr['stop']['route'] as $key => $data) {
+            $data['destination'] = str_replace("CAP SQR", "Capital Square", $data['destination']);
+            $data['destination'] = str_replace("DUNNS MSH", "Dunna Marsh", $data['destination']);
+            $data['destination'] = str_replace("CAP SQR", "Capital Square", $data['destination']);
+            $data['destination'] = str_replace("NORTH TP", "North Transfer Point", $data['destination']);
+            $data['destination'] = str_replace("EAST TP", "East Transfer Point", $data['destination']);
+            $data['destination'] = str_replace("WEST TP", "West Transfer Point", $data['destination']);
+            $data['destination'] = str_replace("SOUTH TP", "South Transfer Point", $data['destination']);
+            $data['destination'] = str_replace("E TOWNE", "East Town", $data['destination']);
+            $data['destination'] = str_replace("WEXFD RG", "Wexford Ridge", $data['destination']);
+            $data['destination'] = str_replace("WESTFLD", "Westfield", $data['destination']);
+            
+            $responseText .= "Route ".$data['routeID']." toward ".$data['destination'];
+            $responseText .= " arrives at ".$data['arrivalTime'].". ";
+        }
+        return $responseText;
+    }
+}
