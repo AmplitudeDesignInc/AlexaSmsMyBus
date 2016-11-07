a0a1c39a57823b7d90984b833db33de9babdfbbf
Kevin Johnson <kevin@amplitudedesign.com>
2016-11-07 15:58:21 -0600

    Check for value of route Number

--- src/Intents/HelpIntent.php
+++ src/Intents/HelpIntent.php
@@ -23,7 +23,7 @@ class HelpIntent
     {
         $this -> responseText = 'Ask for Madison Metro bus and route number information.';
         $this -> responseText .= "\r\n";        
-        $this -> responseText .= 'For example, say, "Hey Alexa, ask Madison Metro for stop number one one zero one route seventy."';
+        $this -> responseText .= 'For example, say, "Hey Alexa, ask Madison Metro for stop number zero one eight zero route forty-seven."';
         $this -> responseText .= "\r\n";
         $this -> responseText .= "We will return upcoming schedule information.";
         $this -> response->respond($this -> responseText)->withCard("Madison Metro Help:" ,$this -> responseText);
